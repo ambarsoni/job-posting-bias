f9dee46f267ceb89a90165d5ac6e78aed66de7a4
Kelly O'Shaughnessy <kelly.oshaughnessy2@gmail.com>
2016-05-16 20:21:05 -0400

    visual overview sans change in bias

--- frontend/js/results.js
+++ frontend/js/results.js
@@ -1,80 +1,116 @@
 //Survey results part I
 
-function init_results(company) {
+function overview_results(batch, company) {
+	var csv_name = "js/survey" + batch + "_overview.csv";
+	d3.csv(csv_name, function(RandCSV){
+		var data = [];
+		var colors = ["pink", "palegreen","lightskyblue"];
 
-	var width = 150,
+		//Populate data
+		RandCSV.forEach(function(companyRow, i) {
+			if(companyRow["COMPANY"] != null) {
+				data.push({
+					name: companyRow["COMPANY"],
+					overall_ranks: [companyRow["NUM1"],
+									companyRow["NUM2"],
+									companyRow["NUM3"],
+									companyRow["NUM4"],
+									companyRow["NUM5"]]
+				});
+			}
+		});
+
+		//Extract relevant company overview data
+		var rel_data = [];
+		data.forEach(function (d,i) {
+			if (d.name == company) {
+				rel_data[0] = d.overall_ranks[0]*1 + d.overall_ranks[1]*1;
+				rel_data[1] = d.overall_ranks[2]*1;
+				rel_data[2] = d.overall_ranks[3]*1 + d.overall_ranks[4]*1;
+			}
+		});
+
+		//Create svg & donut
+		var width = 150,
 	    height = 150,
-	    padding = 10,
+	    padding = 30,
 	    radius = Math.min((width-padding)/2, (height-padding)/2);
 
-	var apple_cnts = [3,8,3]
-	var colors = d3.scale.ordinal()
-				.range(["pink", "palegreen","lightskyblue"]);
+	    var pie = d3.layout.pie().sort(null);
 
-	var pie = d3.layout.pie()
-	    .sort(null);
+	    var arc = d3.svg.arc()
+				    .innerRadius(radius - 0)
+				    .outerRadius(radius - 20);
 
-	var arc = d3.svg.arc()
-	    .innerRadius(radius - 0)
-	    .outerRadius(radius - 20);
+		var svg = d3.select("#" + company).append("svg")
+				    .attr("width", width)
+				    .attr("height", height)
+				    .attr("align","center")
+				    .append("g")
+				    .attr("transform", "translate(" + (width-padding)/2 + "," + (radius) + ")");	
 
-	var svg = d3.select("#apple").append("svg")
-	    .attr("width", width)
-	    .attr("height", height)
-	    .attr("align","center")
-	    .append("g")
-	    .attr("transform", "translate(" + (width-padding)/2 + "," + (radius) + ")");	
-
-	var g = svg.selectAll(".arc")
-				.data(pie(apple_cnts))
-				.enter()
-				.append("path")
-				.attr("fill", function(d,i) { return colors(i); })
-				.attr("d", arc)
-
-	// d3.csv("survey1_overview.csv", function(RandCSV){
-	// 	var colors = ["lightskyblue","pink","palegreen"]
-	// 	var apple = [3,3,3,3,3,3,3,3,4,4,5,1,2]
-	// 	// RandCSV.forEach(function(col, i) {
-	// 	// 	if(col["RESULT"] != null) {
-
-	// 	// 	}
-	// 	// })
-		
-	// 	})
+		var g = svg.selectAll(".arc")
+			.data(pie(rel_data))
+			.enter()
+			.append("path")
+			.attr("fill", function(d,i) { return colors[i]; })
+			.attr("d", arc);
+	})
 }
 
-function post_results(company) {
-
+function comp(company) {
 	var width = 150,
 	    height = 150,
-	    padding = 10,
-	    radius = Math.min((width-padding)/2, (height-padding)/2);
+	    padding = 10;
 
-	var apple_cnts = [3,10,1]
-	var colors = d3.scale.ordinal()
-				.range(["pink", "palegreen","lightskyblue"]);
+	var svg = d3.select("#" + company + "_co").append("svg")
+				.attr("width", width)
+			    .attr("height", height)
+			    .append("g")
+			    ;
 
-	var pie = d3.layout.pie()
-	    .sort(null);
+	var g = svg.append("text")
+				.attr("x", width/2)
+				.attr("y", height/2)
+				.text(company)
+				.style("text-anchor","middle")
+				.style("font-size", "20px")
+				.style("text-decoration","underline")
+				.style("fill","black")
+			;
 
-	var arc = d3.svg.arc()
-	    .innerRadius(radius - 0)
-	    .outerRadius(radius - 20);
+}
 
-	var svg = d3.select("#apple2").append("svg")
-	    .attr("width", width)
-	    .attr("height", height)
-	    .attr("align","center")
-	    .append("g")
-	    .attr("transform", "translate(" + (width-padding)/2 + "," + (radius) + ")");	
+function job(company) {
+	var jobs = {APPLE:"Program Manager",
+				BIRCHBOX: "Developer",
+				ACCENTURE: "Java Developer",
+				REDANTLER: "Web Engineer",
+				PAYPAL: "Java Developer",
+				GOOGLE: "Partner Manager; YouTube",
+				FACEBOOK: "Data Engineer",
+				YAHOO: "Experience Designer",
+				ETSY: "Product Designer",
+				ORACLE: "Developer"};
 
-	var g = svg.selectAll(".arc")
-				.data(pie(apple_cnts))
-				.enter()
-				.append("path")
-				.attr("fill", function(d,i) { return colors(i); })
-				.attr("d", arc);
+	var width = 160,
+	    height = 150,
+	    padding = 10;
+
+	var svg = d3.select("#" + company + "_job").append("svg")
+				.attr("width", width)
+			    .attr("height", height)
+			    .append("g")
+			    ;
+
+	var g = svg.append("text")
+				.attr("x", width/2)
+				.attr("y", height/2)
+				.text(jobs[company])
+				.style("text-anchor","middle")
+				.style("font-size", "12px")
+				.style("fill","black")
+			;
 
 }
 
@@ -84,7 +120,7 @@ function change(company) {
 	    height = 150,
 	    padding = 10;
 
-	var svg = d3.select("#apple_change").append("svg")
+	var svg = d3.select("#" + company + "_change").append("svg")
 				.attr("width", width)
 			    .attr("height", height)
 			    .append("g")
